011c70f15cc61018df31fa4b499445510b6e2de7
Mikkuk <mikko.kukkonenhml@gmail.com>
2022-02-28 18:57:26 +0200

    fix index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.post('/api/persons', (req, res) => {
     res.json(persons)
 })
 
-const PORT = proces.env.PORT;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
